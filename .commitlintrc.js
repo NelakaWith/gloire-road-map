192b87b73ea9a2e31a4575b3cc9fff7ee997016d
NelakaWith <nelaka.with@gmail.com>
2025-09-22 17:02:02 +0530

    fix(commitlint): increase max line lengths for body and header

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [2, "always", 100],
-    "header-max-length": [2, "always", 100],
+    "body-max-line-length": [2, "always", 200],
+    "header-max-length": [2, "always", 200],
     // Allow sentence-case subjects (e.g. "chore: Refactor code structure...")
     "subject-case": [0, "never"],
   },
